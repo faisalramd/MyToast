b7e035eef6eff7d34cb2b3225c81e0be54f2bf79
Faisal <faisalramd@gmail.com>
2018-10-30 08:03:48 +0700

    Initial Library commit

--- mytoast/src/main/java/com/pcpcpcpc/mytoast/MyToast.java
+++ mytoast/src/main/java/com/pcpcpcpc/mytoast/MyToast.java
@@ -3,7 +3,10 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class MyToast {
-    private void show(Context c, String t){
+    public void show(Context c, String t){
         Toast.makeText(c, t, Toast.LENGTH_LONG).show();
+    }
+    public void showMultiplier(Context c, Integer x, Integer y){
+        Toast.makeText(c, ""+(x*y), Toast.LENGTH_LONG).show();
     }
 }
